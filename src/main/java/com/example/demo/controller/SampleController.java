dc427e459e91148cc03ca47fa508ecce84b55dbf
jadha <jadha@DESKTOP-2I3RTA0>
2021-05-03 10:25:12 +0530

    Junit test cases added

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
-	@GetMapping("/")
-	public String getData() {
-		return "Welcome app is running...";		
+	@GetMapping("/sample/{pathVariable}")
+	public String getData(@PathVariable String pathVariable) {
+		return "Welcome app is running..." + pathVariable;		
 	}
 }
--- src/test/java/com/example/demo/SpringBootActuatorApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootActuatorApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootActuatorApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
+	public void testSampleController() throws Exception {
+		RequestBuilder builder = MockMvcRequestBuilders
+					.get("/sample/var1");
+		
+		MvcResult result = mockMvc.perform(builder).andReturn();
+		
+		assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+		assertTrue(result.getResponse().getContentAsString().contains("var1"));
+		
 	}
 
 }
